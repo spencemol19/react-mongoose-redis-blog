1c858de8f4678adebbd54d24cfed09aa732da8f5
Spencer Moulton <spencemoulton@gmail.com>
2018-04-30 12:20:42 -0600

    updated test file...temporarily in broken state

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,8 @@
 const puppeteer = require('puppeteer');
 
-test('Add two numbers', () => {
-    const sum = 1+2;
-
-    expect(sum).toEqual(3);
-});
-
 test('We can launch a browser', async () => {
     const browser = await puppeteer.launch({
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto('localhost:3000');
 });
